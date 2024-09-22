18ba321ee5211a257a128a1d63395568895027f8
Muhammad <hafizmuhammadshah11@gmail.com>
2024-09-22 17:37:39 +0500

    one page

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-$(document).ready(function() {
-    $('.navbar-toggler').click(function() {
-        $(this).toggleClass('active');
-        $('#navbarNav').slideToggle();
-    });
-});
